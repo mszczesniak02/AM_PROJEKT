6f6a25188d0b3d62544c0e800eb0efe6964ebb9b
mszczesniak <mszczesniak@student.agh.edu.pl>
2025-06-16 18:51:48 +0200

    JEBANE GÓWNO KRWA NIE DZIAŁA

--- client/main.c
+++ client/main.c
@@ -18,46 +18,112 @@
 #define PLAYER_MSG "bitches come and go."
 #define PLAYER_GONE "bullshit."
 
+#define AMCOM_OBJECT_TYPES_AMOUNT 4
+#define AMCOM_MAX_OBJECT_TYPES_NAME_LENGTH 12
+
+
+
+
+int DUPA = 0;
+
+typedef struct ObjectNode {
+    AMCOM_ObjectState data;
+    struct ObjectNode* next;
+} ObjectNode_t;
+
 
 
 
 
 typedef struct {
-    float x;
-    float y;
-} ALG_Positions_t;
+    
+    AMCOM_ObjectState objects[AMCOM_MAX_OBJECT_UPDATES];
+    
+    ObjectNode_t* players;
+    ObjectNode_t* static_objects;
 
+    uint8_t objects_max;
+    uint8_t objects_total;
 
+    uint8_t objects_registered;
+    
+    
+    uint8_t players_total;
+    uint8_t player_num;    
+    uint8_t object_follow;
 
-typedef struct {
-        uint8_t objects_init;
-        uint8_t objects_registered;
-
-        AMCOM_ObjectState objects[AMCOM_MAX_OBJECT_UPDATES];
-        
-
-
-        uint8_t objects_total;
-        uint8_t players_total;
-        uint8_t player_num;
-        
 }ALG_GameDetails_t;
 
-static ALG_GameDetails_t *  ALG_GameDetails = NULL; // global register of game details, made global for the sole purpose of not messing up with already-established tcp connectors and other pre-made funcionalities 
+// add new object 
+ALG_GameDetails_t *  ALG_GameDetails = NULL; // global register of game details, made global for the sole purpose of not messing up with already-established tcp connectors and other pre-made funcionalities 
 
-/*
-    struct of the object requires some way of storing the position on a map, and calculating the trejectory(angle)
-*/
+ObjectNode_t *  head_player     = NULL;
+ObjectNode_t *  head_object     = NULL;
 
-// USER FUNCS
+
+
+// objects are unique, they cannot be duplicates
+void ALG_addObject(ObjectNode_t * objectList, AMCOM_ObjectState * object){
+    ObjectNode_t * newObj = (ObjectNode_t * )malloc(sizeof(ObjectNode_t));
+
+    newObj->data = *object;
+    newObj->next = NULL;
+
+    // check if null / head
+    if (objectList == NULL){
+        objectList = newObj;
+    }else{
+        ObjectNode_t * ptr = objectList;
+        while(ptr->next != NULL){
+        printf("pppp?\n");
+
+            ptr = ptr->next;
+        }
+        ptr->next = newObj;
+    }
+
+}
+
+ObjectNode_t* ALG_findObject(ObjectNode_t * objectList, uint8_t object_type, uint8_t object_num){
+    ObjectNode_t* result = NULL;
+    ObjectNode_t* ptr = objectList;
+    while(ptr != NULL){
+        if (ptr->data.objectNo == object_num && ptr->data.objectType == object_type){
+            return ptr;
+        }else{
+            ptr = ptr->next;
+        }
+    }
+
+    printf("Object not found!\n");
+    return result;
+}
+
+void ALG_updateObject(ObjectNode_t * objectList, uint8_t object_type, uint8_t object_num, AMCOM_ObjectState * theUpdate){
+
+    ObjectNode_t * object = ALG_findObject(objectList, object_type, object_num);
+    if (object == NULL){
+        printf("Object not in the list, could not update");
+    }else{
+        object->data = *theUpdate;
+        // memcpy(&(object->data), theUpdate, sizeof(AMCOM_ObjectState));
+    }
+
+}
+
+
 
 void ALG_GameDetailsInit(){
 
-    ALG_GameDetails->objects_registered     = 0;
-    ALG_GameDetails->objects_init           = 0;
     ALG_GameDetails->objects_total          = 0;
     ALG_GameDetails->player_num             = 0;
     ALG_GameDetails->players_total          = 0;
+    ALG_GameDetails->object_follow          = 0;
+    ALG_GameDetails->objects_max = AMCOM_MAX_OBJECT_UPDATES;
+    
+    ALG_GameDetails->static_objects = head_object;
+    ALG_GameDetails->players        = head_player;
+
     
     memset(ALG_GameDetails->objects, 0, sizeof(ALG_GameDetails->objects));
 };
@@ -86,25 +152,79 @@ float ALG_GetRelativeAngle(AMCOM_ObjectState * object_origin, AMCOM_ObjectState 
     return angle ;
 }
 
-void ALG_Transpose(AMCOM_ObjectState * object){ //transpose from -500/500 to 0/1000
-    object->x += 500.0f;
-    object->y += 500.0f;
+
+int ALG_CheckList(AMCOM_ObjectState * object){ // linear check for existance in the object register
+    AMCOM_ObjectState * objects = ALG_GameDetails->objects;
+
+    for (uint8_t i = 0; i< ALG_GameDetails->objects_max; ++i){
+        if (objects->objectNo == object->objectNo && 
+        objects->objectType == object->objectType){
+            return 1;
+        }
+
+    }
+    return 0;
 }
 
 void ALG_FillGamedata(AMCOM_NewGameRequestPayload * game_data){
+
+    printf("\nplayers_total: %u, player_num:%u.\n", game_data->numberOfPlayers, game_data->playerNumber);
+
     ALG_GameDetails->players_total = game_data->numberOfPlayers;
     ALG_GameDetails->player_num = game_data->playerNumber;
-    ALG_GameDetails->objects_init = 1;
 }
 
 void ALG_FillObjects(AMCOM_ObjectState * object, uint8_t object_amount){
     for(uint8_t i = 0 ; i<object_amount; ++i){
-        ALG_GameDetails->objects[i] = object[i];
-        ALG_Transpose(&ALG_GameDetails->objects[i]);
-
+        // check if object exists, if it does, dont put it in
+        if(!ALG_CheckList(&object[i])){
+            ALG_GameDetails->objects[i] = object[i];
+        }
     }
-    ALG_GameDetails->objects_total = object_amount;
-    ALG_GameDetails->objects_registered = 1;
+    ALG_GameDetails->objects_total += object_amount;
+}
+
+void ALG_FillObjects2(AMCOM_ObjectState * object, uint8_t object_amount){
+    for(uint8_t i = 0 ; i<object_amount; ++i){
+        // check if object exists, if it does, dont put it in
+        if (object->objectType == 0){
+            
+            ObjectNode_t * ptr;
+            if ((ptr = ALG_findObject(ALG_GameDetails->players, object->objectType, object->objectNo)) == NULL){
+                ALG_addObject(  ALG_GameDetails->players, object);
+            }else{
+                ALG_updateObject(ALG_GameDetails->players,object->objectType, object->objectNo, object );
+            }
+        }else{
+            ALG_addObject(ALG_GameDetails->static_objects, object);
+        }
+    }
+    ALG_GameDetails->objects_total += object_amount;
+}
+
+
+
+
+void ALG_PrintObject(AMCOM_ObjectState * object){
+        
+    printf("\nO.number: %u \n",object->objectNo );
+    printf("O.type:   (%u) \n" , object->objectType );
+    printf("O.HP:     %d \n",object->hp );
+    printf("O.POSX:   %f \n",object->x );
+    printf("O.POSY:   %f \n\n",object->y );
+
+}
+void printA(){
+    ObjectNode_t * ptr = ALG_GameDetails->static_objects;
+    while(ptr != NULL){
+        ALG_PrintObject(&ptr->data);
+        ptr = ptr->next;
+    }
+    ptr = ALG_GameDetails->players;
+    while(ptr != NULL){
+        ALG_PrintObject(&ptr->data);
+        ptr = ptr->next;
+    }
 }
 
 void ALG_PrintObjects(void){
@@ -112,9 +232,6 @@ void ALG_PrintObjects(void){
         uint8_t type_num;
 
         for (uint8_t i = 0; i< ALG_GameDetails->objects_total ; ++i){
-            
-
-
             type_num = ALG_GameDetails->objects[i].objectType;
             
             printf("\nO.number: %u \n",ALG_GameDetails->objects[i].objectNo );
@@ -126,49 +243,21 @@ void ALG_PrintObjects(void){
         }
 }
 
-AMCOM_ObjectState * ALG_FindPlayer(void) {
-    AMCOM_ObjectState * ptr = ALG_GameDetails->objects;
-    
+AMCOM_ObjectState * ALG_FindObject(uint8_t object_type, uint8_t object_number) {
     for (uint8_t i = 0; i < ALG_GameDetails->objects_total; ++i) {
-        if(ptr->objectType == 0) {  // Sprawdź czy to jest PLAYER (typ 0)
-            return ptr;
-        }
-        ptr++;
-    }
-    return NULL;  // Jeśli nie znaleziono gracza
-}
-AMCOM_ObjectState* ALG_FindObject(uint8_t object_type){
-    AMCOM_ObjectState * ptr = ALG_GameDetails->objects;
-    
-    // uint16_t number = (uint16_t)ALG_GameDetails->;
-    
-    for (uint8_t i = 0 ; i < ALG_GameDetails->objects_total; ++i){
-        if(ptr->objectNo == object_type){
-            return ptr;
-        }else{
-            ptr++;
-        }
-    }
-    return ptr;
-}
+        printf("\nTrying to find object: %u num %u.\n", object_type, object_number);
 
-AMCOM_ObjectState * ALG_FindNearestObject(uint8_t object_type){
-    AMCOM_ObjectState * player = ALG_FindPlayer();
-    AMCOM_ObjectState * nearest_object = NULL;
-    float min_distance = 10000.0f;
-    for (uint8_t i = 0; i< ALG_GameDetails->objects_total; ++i){
-        AMCOM_ObjectState * current_object = &ALG_GameDetails->objects[i];
-        if (current_object->objectType ==object_type ) {
-            float distance = ALG_GetDistance(player,current_object);
-            if( distance < min_distance){
-                min_distance = distance;
-                nearest_object  = current_object;
-            }
+        if (ALG_GameDetails->objects[i].objectType == object_type && 
+            ALG_GameDetails->objects[i].objectNo == object_number) {
+            printf("\nObject found: (x,y) = (%f, %f).\n", 
+                   ALG_GameDetails->objects[i].x, 
+                   ALG_GameDetails->objects[i].y);
+            return &ALG_GameDetails->objects[i];
         }
     }
-    return nearest_object;
+    printf("ALG_FindObject(): Object not found.\n");
+    return NULL;  // If object not found
 }
-
 
 void AMCOM_Print(AMCOM_ObjectState * current_object, uint8_t objects_amount){
    char * types[]  = {"PLAYA","TRANSISTOR", "SPARK", "GLUE"};
@@ -191,6 +280,8 @@ void AMCOM_Print(AMCOM_ObjectState * current_object, uint8_t objects_amount){
 
 // USER END
 
+
+
 void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
 
     uint8_t buf[AMCOM_MAX_PACKET_SIZE];              // buffer used to serialize outgoing packets
@@ -199,8 +290,10 @@ void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
 
     switch (packet->header.type) {
         case AMCOM_IDENTIFY_REQUEST:
-            printf("IDENTIFY.request. Responding with %s.\n", PLAYER_NAME);
+            
             AMCOM_IdentifyResponsePayload identifyResponse;
+            printf("IDENTIFY.request. Responding with %s.\n", PLAYER_NAME);
+            
             sprintf(identifyResponse.playerName, PLAYER_NAME);
             toSend = AMCOM_Serialize(AMCOM_IDENTIFY_RESPONSE, &identifyResponse, sizeof(identifyResponse), buf);
             break;
@@ -208,8 +301,9 @@ void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
         case AMCOM_NEW_GAME_REQUEST:
             printf("NEW_GAME.request. Responding with %s.\n", PLAYER_MSG);
 
+            // GET THE DATA FROM A PACKET AND INSERT THE IN TO REGISTER
             AMCOM_NewGameRequestPayload * gameData =(AMCOM_NewGameRequestPayload *)packet->payload ;
-            if (ALG_GameDetails->objects == 0) ALG_FillGamedata(gameData);
+            ALG_FillGamedata(gameData);
 
 
             // send player name
@@ -232,13 +326,11 @@ void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
 
 
             uint8_t objects_amount = packet->header.length / (uint8_t)sizeof(AMCOM_ObjectState);
-            AMCOM_ObjectState * current_object = (AMCOM_ObjectState *)packet->payload;
+            AMCOM_ObjectState * objects = (AMCOM_ObjectState *)packet->payload;
             
-            if (ALG_GameDetails->objects_registered == 0) ALG_FillObjects(current_object, objects_amount);
-            
-            // ALG_PrintObjects();
-            // AMCOM_Print(current_object, objects_amount);
-         
+            ALG_FillObjects(objects, objects_amount);
+            ALG_FillObjects2(objects, objects_amount);
+            // printA();
             /*
                 TO DO:
                 ANALYZE WHAT TO DO:
@@ -251,18 +343,27 @@ void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
         case AMCOM_MOVE_REQUEST:
             printf("MOVE.request. Responding with MOVING\n");
 
-            AMCOM_MoveResponsePayload moveResponse;
-            
-            AMCOM_ObjectState * player =  ALG_FindPlayer();
-            AMCOM_ObjectState * target = ALG_FindNearestObject(1);
-            if(target != NULL){
-                moveResponse.angle = ALG_GetRelativeAngle(player, target  );
-            }else{
-                // moveResponse.angle = 1.4f;
-            }
 
+            AMCOM_MoveResponsePayload moveResponse;
+            // ALG_PrintObjects();
             
-            toSend = AMCOM_Serialize(AMCOM_MOVE_RESPONSE, &moveResponse, sizeof(moveResponse), buf);
+
+            // AMCOM_ObjectState * player =  ALG_FindObject(0, ALG_GameDetails->player_num);   // 0 - player
+            // if(player != NULL){
+                    
+                
+            //     if ( DUPA > 2){
+            //         AMCOM_ObjectState * object =  ALG_FindObject(1, 0);            // 1 - transistor 
+            //         float new_angle = ALG_GetRelativeAngle(player,object );
+            //         printf("newAngle %f\n", new_angle);
+            //     }else{
+            //         DUPA++;
+            //     }
+            //     // try to calculate angle
+                moveResponse.angle = 1;
+    
+                toSend = AMCOM_Serialize(AMCOM_MOVE_RESPONSE, &moveResponse, sizeof(moveResponse), buf);
+            }
             break;
 
       
@@ -287,7 +388,8 @@ void amPacketHandler(const AMCOM_Packet* packet, void* userContext) {
 int main(int argc, char **argv) {
 
     ALG_GameDetails = (ALG_GameDetails_t * )malloc(sizeof(ALG_GameDetails_t));
-
+    
+    ALG_GameDetailsInit();
 
     printf("This is mniAM player. Let's eat some transistors! \n");
 
@@ -380,8 +482,10 @@ int main(int argc, char **argv) {
     } while( iResult > 0 );
 
     // No longer need the socket
-    
+    free(head_object);
+    free(head_player);
     free(ALG_GameDetails);
+
     closesocket(ConnectSocket);
     // Clean up
     WSACleanup();
